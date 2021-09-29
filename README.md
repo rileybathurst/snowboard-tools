033aab6c93b93f276178767a149d07fd36b784f1
Riley Bathurst <riley@rileybathurst.com>
2021-09-29 14:03:22 -0700

    test M

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # Snowboard Tools
 
 Playing around with creating some online tools in react.
+
+This is starting to come from https://github.com/rileybathurst/board-size and this https://github.com/rileybathurst/snowboard-size
+
+I don't know where I had all the math best sorted
+boardsize is newer but I'm not sure if thats the thing
+lets try and hope it is
+seems like more of a second title??? soo?
+
+base math so it's here in clean english?
+
+I'm going to work in metric first then convert everything to imperial as its smarter
+so all this is written in metric
+it also outputs in cm which is key.
+
+## height
+
+this is key
+times by 0.885 is board size
+Start the height at 175cm
+that starts the board guess at 155cm
+
+## weight
+
+Guess the weight at 0.4 * the height
+any difference in weight is *0.3 
+70kg is the guess
+so droping the weight by 10kg is a 3cm difference in board size
+
+## mountain size
+
+math is litterally plus and minus 1cm at the moment that can be way better
+
+## final size
+
+(height * 0.885) + weightOffset + (mountain * 1)
+then rounded to cm.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,66 @@
-import * as React from "react"
+import React, { useState } from 'react';
 import FooterSection from "../components/footer";
 import HeaderSection from "../components/header";
 
 import "../styles/app.scss";
 
+
+
+function Size() {
+
+  // start the state variables
+  const [size, setSize] = useState(155);
+  const [height, setHeight] = useState(175);
+
+  console.log('hey');
+
+  function mest(e) {
+    console.log(e.target.value);
+    console.log('here');
+    return null;
+  }
+
+  return (
+    <>
+      <h2>Snowboard Size Guide</h2>
+      <form>
+        <label>
+          Height
+        </label>
+        Test M
+        <input
+          type="range"
+          onChange={mest}
+        />
+
+        <input
+          type="range"
+          onChange={mest}
+        />
+        <p>&#60;&#60;&#60; slide &#62;&#62;&#62;</p>
+
+        <label>
+          Weight
+        </label>
+        {/* <input type="range" /> */}
+        <p>&#60;&#60;&#60; slide &#62;&#62;&#62;</p>
+
+        {/* <input type="number" value={size} /> */}
+      </form>
+    </>
+  );
+}
+
 // markup
 const IndexPage = () => {
   return (
     <>
-    <HeaderSection />
-    <main>
-      <title>Snowboard.Tools</title>
-        <h2>Snowboard Size Guide</h2>
-        <form>
-          <label>
-            Height
-          </label>
-          <input type="range" />
-          <p>&#60;&#60;&#60; slide &#62;&#62;&#62;</p>
-          <label>
-            Weight
-          </label>
-          <input type="range" />
-          <p>&#60;&#60;&#60; slide &#62;&#62;&#62;</p>
-        </form>
-    </main>
-    <FooterSection />
+      <HeaderSection />
+      <main>
+        <title>Snowboard.Tools</title>
+        <Size />
+      </main>
+      <FooterSection />
     </>
   )
 }
